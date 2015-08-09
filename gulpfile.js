a5dc2126f76d314599171ff0cf8f8be9cd700d26
Pieter Delbeke <pieter.delbeke@zentrick.com>
2015-08-09 12:48:17 +0200

    Improved build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
 var gulp = require('gulp')
 var sourcemaps = require('gulp-sourcemaps')
 var babel = require('gulp-babel')
-// var concat = require('gulp-concat')
 
-gulp.task('default', function () {
+gulp.task('dev', function () {
   return gulp.src('src/**/*.es6')
     .pipe(sourcemaps.init())
     .pipe(babel())
-    // .pipe(concat('motu.js'))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('build'))
+})
+
+gulp.task('default', ['dev'])
+
+gulp.task('watch', ['dev'], function () {
+  gulp.watch('src/**/*.es6', ['dev'])
 })
